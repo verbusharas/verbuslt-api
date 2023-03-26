b771dedde9edd6572139cce142978f4fb93652b9
Šarūnas Verbus <sarunas@inventi.lt>
2023-03-26 16:48:27 +0300

    add index endpoint

--- src/main/kotlin/lt/verbus/verbusltapi/web/controller/TestController.kt
+++ src/main/kotlin/lt/verbus/verbusltapi/web/controller/TestController.kt
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class TestController {
 
+    @GetMapping("/")
+    fun getIndex() = "Index"
+
     @GetMapping("/hello")
     fun greet() = "Hi"
 }
